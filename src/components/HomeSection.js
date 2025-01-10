7bd3fe7296bc3bcd0cd005ed873f65d7612187c8
Mukesh <lakkakulababblu@gmail.com>
2025-01-10 18:29:31 +0530

    updated desktop view

--- src/components/HomeSection.js
+++ src/components/HomeSection.js
@@ -1,30 +1,52 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import NavBar from "./NavBar";
 import { motion } from "framer-motion";
 import Image from "next/image";
-import centerimage from "../../public/Images/centerimage.jpeg"
+import centerimage from "../../public/Images/centerimage.jpeg";
 import { FaLinkedin } from "react-icons/fa";
-import { FaSquareXTwitter } from "react-icons/fa6";import { FaInstagram } from "react-icons/fa6";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa6";
 const HomeSection = (theme) => {
+  console.log("thge", theme.theme);
+  useEffect(() => {}, [theme]);
   return (
-    <motion.div id="home" className="min-h-screen w-full flex justify-center items-center" animate={{ opacity: 1 }}
-    transition={{ duration: 0.5 }}>
+    <motion.div
+      id="home"
+      className="min-h-screen w-full flex justify-center items-center"
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+    >
       <NavBar theme={theme} />
       <div className="w-full lg:w-[60%]  flex flex-col lg:flex-row justify-between items-center pb-[60px] lg:pb-[80px]">
-<div className="text-white flex lg:flex-col flex-row justify-between items-center gap-2 lg:gap-20">
-  <p className="lg:rotate-90">follow on</p>
-  <hr className="w-[100px] lg:rotate-90 block"/>
-  <div>
-  <FaLinkedin /><FaSquareXTwitter /><FaInstagram />
-  </div>
-  
+        <div
+          className={`${
+            !theme.theme ? "text-white" : "text-black"
+          }text-red-500 flex lg:flex-col flex-row justify-between items-center gap-2 lg:gap-20`}
+        >
+          <p
+            className={`${
+              !theme.theme ? "text-black" : "text-white"
+            } lg:rotate-90`}
+          >
+            follow on
+          </p>
+          <hr className="w-[100px] lg:rotate-90 block" />
+          <div>
+            <FaLinkedin />
+            <FaSquareXTwitter />
+            <FaInstagram />
+          </div>
+        </div>
 
-</div>
-      
-      <Image src={centerimage} alt="centerImage" className="rounded-full w-[70%] lg:w-[30%] m-auto"/>
-      </div> 
-       </motion.div>
+        <Image
+          src={centerimage}
+          alt="centerImage"
+          className="rounded-full w-[70%] lg:w-[30%] m-auto"
+        />
+        <div>scroll to down</div>
+      </div>
+    </motion.div>
   );
 };
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,75 +9,83 @@ import { MdHomeRepairService } from "react-icons/md";
 import { LuGripHorizontal } from "react-icons/lu";
 
 const NavBar = (theme) => {
-  const [navBarActive,setNavbarActive] = useState(true)
+  const [navBarActive, setNavbarActive] = useState(true);
   const scrollToSection = (id) => {
     document.getElementById(id)?.scrollIntoView({ behavior: "smooth" });
   };
   const navItems = [
-      { id: "home", label: "Home", icon: <FaHome /> },
-      { id: "about", label: "About", icon: <BiSolidUser /> },
-      { id: "resume", label: "Resume", icon: <GrCertificate /> },
-      { id: "services", label: "Service", icon: <LuHeartHandshake /> },
-      { id: "portfolio", label: "Portfolio", icon: <MdHomeRepairService /> },
-      { id: "contact", label: "Contact", icon: <LuMessageCircleMore /> },
-    ];
+    { id: "home", label: "Home", icon: <FaHome /> },
+    { id: "about", label: "About", icon: <BiSolidUser /> },
+    { id: "resume", label: "Resume", icon: <GrCertificate /> },
+    { id: "services", label: "Service", icon: <LuHeartHandshake /> },
+    { id: "portfolio", label: "Portfolio", icon: <MdHomeRepairService /> },
+    { id: "contact", label: "Contact", icon: <LuMessageCircleMore /> },
+  ];
   return (
     <div>
       {/* <!-- Bottom Navigation Menu --> */}
       <div className="bottom-nav-container">
         <div className="bottom-nav">
           <div className="bottom-nav-inner relative ">
-            {navBarActive?
+            {navBarActive ? (
               <ul
-              className={`flex justify-between min-w-[94%] text-white md:text-black md:min-w-[350px] items-center bottom-4 bg-gray-500 fixed md:bg-gray-300 pl-4 pr-4 pb-2 pt-2 rounded-full left-1/2 transform -translate-x-1/2 transition duration-150  ease-in-out   ${
-                navBarActive ? "opacity-100 scale-100" : "opacity-0 scale-90"
-              }`}
-            > {navItems.map((item) => (
-              <li key={item.id} className="menu-item relative group">
-                <div
-                   onClick={() => scrollToSection(item.id)}
-                  className={`group-hover:text-white text-xl group-hover:bg-red-500 rounded-full p-2 ${
-                    theme.theme ? "group-hover:text-white" : ""
-                  }`}
-                >
-                  {item.icon}
-                </div>
-
-                <span className="absolute top-[-40px] left-1/2 transform -translate-x-1/2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-white text-sm bg-red-500 py-1 px-2 rounded-lg shadow-lg pointer-events-none">
-                  {item.label}
-                  <svg
-                    className="absolute left-1/2 transform -translate-x-1/2 bottom-[-5px] fill-current text-red-500"
-                    width="10"
-                    height="5"
-                    viewBox="0 0 10 5"
-                    xmlns="http://www.w3.org/2000/svg"
+                className={`flex justify-between min-w-[94%] text-white md:text-black md:min-w-[350px] items-center bottom-4 bg-gray-500 fixed md:bg-gray-300 pl-4 pr-4 pb-2 pt-2 rounded-full left-1/2 transform -translate-x-1/2 transition duration-150  ease-in-out   ${
+                  navBarActive ? "opacity-100 scale-100" : "opacity-0 scale-90"
+                }`}
+              >
+                {" "}
+                {navItems.map((item) => (
+                  <li
+                    key={item.id}
+                    className="menu-item relative group"
+                    onClick={() => scrollToSection(item.id)}
                   >
-                    <polygon points="0,0 10,0 5,5" />
-                  </svg>
-                </span>
-              </li>
-            ))} 
-              <li onClick={()=>{setNavbarActive(!navBarActive)}} className="absolute text-white  top-[-5px] hover:text-xl hover:font-extrabold z-10 right-0 transform translate-x-1/4 translate-y-1/10 bg-red-500 p-1 rounded-full">
-                <RxCross2 />
-              </li>
-            </ul>:
-            <div className="fixed bottom-4 left-0 right-0 flex justify-center">
-            <button
-              onClick={() => {
-                setNavbarActive(!navBarActive);
-              }}
-              className="text-3xl text-red-500 items-center bg-gray-400 md:bg-gray-300 pl-4 pr-4 pb-2 pt-2 rounded transition-all duration-500 animate-shake"
-            >
-              <LuGripHorizontal />
-            </button>
-          </div>
-          }
-            
-            
+                    <div
+                      className={`group-hover:text-white text-xl group-hover:bg-red-500 rounded-full p-2 ${
+                        theme.theme ? "group-hover:text-white" : ""
+                      }`}
+                    >
+                      {item.icon}
+                    </div>
+
+                    <span className="absolute top-[-40px] left-1/2 transform -translate-x-1/2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-white text-sm bg-red-500 py-1 px-2 rounded-lg shadow-lg pointer-events-none">
+                      {item.label}
+                      <svg
+                        className="absolute left-1/2 transform -translate-x-1/2 bottom-[-5px] fill-current text-red-500"
+                        width="10"
+                        height="5"
+                        viewBox="0 0 10 5"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <polygon points="0,0 10,0 5,5" />
+                      </svg>
+                    </span>
+                  </li>
+                ))}
+                <li
+                  onClick={() => {
+                    setNavbarActive(!navBarActive);
+                  }}
+                  className="absolute text-white  top-[-5px] hover:text-xl hover:font-extrabold z-10 right-0 transform translate-x-1/4 translate-y-1/10 bg-red-500 p-1 rounded-full"
+                >
+                  <RxCross2 />
+                </li>
+              </ul>
+            ) : (
+              <div className="fixed bottom-4 left-0 right-0 flex justify-center">
+                <button
+                  onClick={() => {
+                    setNavbarActive(!navBarActive);
+                  }}
+                  className="text-3xl text-red-500 items-center bg-gray-400 md:bg-gray-300 pl-4 pr-4 pb-2 pt-2 rounded transition-all duration-500 animate-shake"
+                >
+                  <LuGripHorizontal />
+                </button>
+              </div>
+            )}
           </div>
         </div>
       </div>
-      
     </div>
   );
 };
